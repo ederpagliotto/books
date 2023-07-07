992a34edbeffe6e07dac9eaccb17f07116969e15
Eder Pagliotto <ederpagliotto@gmail.com>
2023-07-07 17:50:25 +0100

    Creating client side (consult)

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const Books = () => {
-  return <div>books</div>;
+  const [books, setBooks] = React.useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get('http://localhost:8800/books');
+        setBooks(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Books Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book">{book.cover && <img />}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Books;
